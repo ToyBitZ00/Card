e65bfd0a37ea2f2dd31ba5a32f1e3ae2dd08b4c9
ToyBitZ00 <pa6308191@gmail.com>
2025-09-08 23:01:47 +0800

    Added some details, improved the user input statements used if statements to catch invalid number inputs, added two random number variables using Math random generator that will be ued later.

--- TestCard.java
+++ TestCard.java
@@ -8,19 +8,42 @@ public class TestCard {
 
         char cardIndex1;
         char cardIndex2;
-        String card;
-        int random1;
-        int random2;
+        String yourCard1;
+        String yourCard2;
+        int yourCardNum1;
+        int yourCardNum2;
+        int fCardRandom;
+        int rCardRandom;
 
-        System.out.print("Choose your first card: ");
-        card = input.next();
-        cardIndex1 = card.charAt(0);
+        System.out.println("0.2% is the probability that both of your cards will match AI's both cards.");
+        System.out.println("But its never 0%. Let's see if you are the lucky one.\n");
+
+        System.out.println("S - Spades");
+        System.out.println("C - Clovers");
+        System.out.println("D - Diamonds");
+        System.out.println("H - Hearts");
+
+        System.out.print("\nChoose your first card: ");
+        yourCard1 = input.next().toUpperCase();
+        System.out.print("Choose your first card's number [1 - 13]: ");
+        yourCardNum1 = input.nextInt();
+        if (yourCardNum1 >= 14) {
+            System.out.println("Nonexistent Card Number choose 1 - 13 only");
+            return;
+        }
 
         System.out.print("Choose your second card: ");
-        card = input.next();
-        cardIndex2 = card.charAt(0);
+        yourCard2 = input.next().toUpperCase();
+        System.out.print("Choose your second card's number [1 - 13]: ");
+        yourCardNum2 = input.nextInt();
+        if (yourCardNum2 >= 14) {
+            System.out.println("Nonexistent Card Number choose 1 - 13 only");
+            return;
+        }
 
-        random1 = (int) (Math.random() * 100 % 13 + 1);
-        random2 = (int) (Math.random() * 100 % 13 + 1);
+        fCardRandom = (int) (Math.random() * 100 % 13 + 1);
+        rCardRandom = (int) (Math.random() * 100 % 13 + 1);
+
+        input.close();
     }
 }
