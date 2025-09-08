ffb860e3fe0845819bcc918bf318d92265ab6585
ToyBitZ00 <pa6308191@gmail.com>
2025-09-09 00:29:34 +0800

    Added a really confusing chain of nested if statements, but all it does is validate of any of your cards, Suits or Number matches the opponents suits and numbers.

--- TestCard.java
+++ TestCard.java
@@ -15,8 +15,8 @@ public class TestCard {
 
         char[] suits = { 'S', 'C', 'D', 'H' };
 
-        int fCardNumRandomAI;
-        int sCardNumRandomAI;
+        int fCardRandomOpp;
+        int sCardRandomOpp;
 
         System.out.println("0.2% is the probability that both of your cards will match AI's both cards.");
         System.out.println("But its never 0%. Let's see if you are the lucky one.\n");
@@ -30,12 +30,14 @@ public class TestCard {
         yourCard1 = input.next().toUpperCase();
         if (!yourCard1.equals("S") && !yourCard1.equals("C") && !yourCard1.equals("D") && !yourCard1.equals("H")) {
             System.out.println("Invalid Card Suits");
+            input.close();
             return;
         }
         System.out.print("Choose your first card's number [1 - 13]: ");
         yourCardNum1 = input.nextInt();
         if (yourCardNum1 >= 14) {
             System.out.println("Nonexistent Card Number choose 1 - 13 only");
+            input.close();
             return;
         }
 
@@ -43,12 +45,14 @@ public class TestCard {
         yourCard2 = input.next().toUpperCase();
         if (!yourCard2.equals("S") && !yourCard2.equals("C") && !yourCard2.equals("D") && !yourCard2.equals("H")) {
             System.out.println("Invalid Card Suits");
+            input.close();
             return;
         }
         System.out.print("Choose your second card's number [1 - 13]: ");
         yourCardNum2 = input.nextInt();
         if (yourCardNum2 >= 14) {
             System.out.println("Nonexistent Card Number choose 1 - 13 only");
+            input.close();
             return;
         }
 
@@ -57,14 +61,45 @@ public class TestCard {
         char randomSuit1 = suits[index1];
         char randomSuit2 = suits[index2];
 
-        fCardNumRandomAI = (int) (Math.random() * 100 % 13 + 1);
-        sCardNumRandomAI = (int) (Math.random() * 100 % 13 + 1);
+        fCardRandomOpp = (int) (Math.random() * 100 % 13 + 1);
+        sCardRandomOpp = (int) (Math.random() * 100 % 13 + 1);
 
-        System.out.println("The AI's first card suit is: " + randomSuit1);
-        System.out.println("The Ai's first card number is: " + fCardNumRandomAI);
+        System.out.println("\nThe AI's first card suit is: " + randomSuit1);
+        System.out.println("The Ai's first card number is: " + fCardRandomOpp);
         System.out.println("The AI's second card suit is: " + randomSuit2);
-        System.out.println("The AI's second card number is: " + sCardNumRandomAI);
+        System.out.println("The AI's second card number is: " + sCardRandomOpp);
 
+        // This checks if both of your card's suits matched the opponents cards
+        if (yourCard1.equals(randomSuit1) && yourCard2.equals(randomSuit2)
+                || yourCard1.equals(randomSuit2) && yourCard2.equals(randomSuit1)) {
+            // This checks if both of your cards number matches the opponents numbers
+            if (yourCardNum1 == fCardRandomOpp && yourCardNum2 == sCardRandomOpp
+                    || yourCardNum1 == sCardRandomOpp && yourCardNum2 == fCardRandomOpp) {
+                System.out.println("\nCONGRATULATIONS YOU MATCHED THE OPPONENTS's BOTH SUITS AND NUMBERS");
+            }
+            // This checks if any of your card's numbers matches the opponents card's
+            // numbers
+            else if ((yourCardNum1 == fCardRandomOpp || yourCardNum2 == sCardRandomOpp)) {
+                System.out.println("YOU MATCHED ONE OF THE OPPONENT's CARD WITH SUITS AND NUMBER");
+                System.out.println("TRY AGAIN LATER");
+            } else {
+                System.out.println("SORRY YOU ONLY MATCHED BOTH OF OPPONENT's CARDS WITH SUITS BUT NOT NUMBERS");
+                System.out.println("TRY AGAIN LATER");
+            }
+        }
+        // This checks if one of your card's suits matches the opponents card's suits
+        else if ((yourCard1.equals(randomSuit1) || yourCard2.equals(randomSuit2))
+                || (yourCard1.equals(randomSuit2) || yourCard2.equals(randomSuit1))) {
+            // This checks if one of your card's number matches the opponents card's number
+            if ((yourCardNum1 == fCardRandomOpp) || (yourCardNum2 == sCardRandomOpp)) {
+                System.out.println("YOU MATCHED ONE THE OPPONENTS's CARD WITH BOTH SUITS AND NUMBERS");
+            } else {
+                System.out.println("SORRY YOU ONLY MATCHED ONE OF THE OPPONENT's CARD WITH SUITS");
+                System.out.println("TRY AGAIN LATER");
+            }
+        } else {
+            System.out.println("SORRY YOU DIDN'T MATCH ANY OF THE OPPONENT's CARDS ");
+        }
         input.close();
     }
 }
