56cb2ce11641e5d842b4694c6be44bcf3aacda6e
ToyBitZ00 <pa6308191@gmail.com>
2025-09-08 22:37:28 +0800

    Added random int variables and used Math.random library to generate random numbers ranging from 1-13 to set a card's number.

--- TestCard.java
+++ TestCard.java
@@ -6,16 +6,21 @@ public class TestCard {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        char card1;
-        char card2;
+        char cardIndex1;
+        char cardIndex2;
         String card;
+        int random1;
+        int random2;
 
         System.out.print("Choose your first card: ");
         card = input.next();
-        card1 = card.charAt(0);
+        cardIndex1 = card.charAt(0);
 
         System.out.print("Choose your second card: ");
         card = input.next();
-        card2 = card.charAt(0);
+        cardIndex2 = card.charAt(0);
+
+        random1 = (int) (Math.random() * 100 % 13 + 1);
+        random2 = (int) (Math.random() * 100 % 13 + 1);
     }
 }
